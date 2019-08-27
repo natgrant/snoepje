fbf39e926f6dfa1018b23d9fc534322bfb355d53
Nat Grant <email.natgrant@gmail.com>
2019-08-27 15:20:45 +1200

    text to be amended about page

--- src/About.jsx
+++ src/About.jsx
@@ -6,8 +6,14 @@ export default function About() {
   return (
     <Fragment>
       <Header />
-      <div className="about-container container has-text-centered">
-        <div className="about">About</div>
+      <div className="about-container container has-text-right">
+        <div className="about-text">
+          <p>
+            <strong>SNOEPje</strong> is here to deliver lovingly brewed natural
+            drinks, each batch is individually crafted using the best quality
+            ingredients nature has to offer.
+          </p>
+        </div>
       </div>
     </Fragment>
   );
